996c7822b52baaa96b193e7e1641fb6947158ca8
Tom <tom-vautray@hotmail.fr>
2021-10-10 22:22:27 +0200

    init

---
+++ src/main/java/jpaentitor/EntityControllerApplication.java
@@ -0,0 +1,13 @@
+package jpaentitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpaentitor/EntityControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package jpaentitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
